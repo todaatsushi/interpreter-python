b3a3f048569d647f861882eee2c76780f91ab70a
Atsushi Toda <todaatsushi0@gmail.com>
2024-11-24 15:04:33 +0000

    Fix interface on lexer

--- interpreter/lexer.py
+++ interpreter/lexer.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import dataclasses as dc
 
+from interpreter import tokens
+
 
 @dc.dataclass(frozen=True)
 class Lexer:
     @classmethod
-    def new(cls) -> Lexer:
+    def new(cls, input: str) -> Lexer:
         raise NotImplementedError
 
-    def next_token(self) -> str:
+    def next_token(self) -> tokens.Token:
         raise NotImplementedError
