cb060d1d257ba3fd8a34727baa85dc61f436cb5b
Atsushi Toda <todaatsushi0@gmail.com>
2024-11-24 15:08:45 +0000

    Fix lexer interface

--- interpreter/lexers.py
+++ interpreter/lexers.py
@@ -5,11 +5,16 @@ import dataclasses as dc
 from interpreter import tokens
 
 
-@dc.dataclass(frozen=True)
+@dc.dataclass
 class Lexer:
+    input: str
+    position: int
+    read_position: int
+    char: bytes | None
+
     @classmethod
     def new(cls, input: str) -> Lexer:
-        raise NotImplementedError
+        return cls(input=input, position=0, read_position=0, char=None)
 
     def next_token(self) -> tokens.Token:
         raise NotImplementedError
