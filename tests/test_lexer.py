7f0fee4f9dd81e2c4c2ad57a67b15a92a08e641f
Atsushi Toda <todaatsushi0@gmail.com>
2024-11-30 15:30:50 +0000

    Uncomment test

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -24,37 +24,37 @@ class TestNextToken(unittest.TestCase):
         self.assertEqual(lexer.read_position, 2)
         self.assertEqual(lexer.literal, None)
 
-    # def test_parse_token(self) -> None:
-    #     lexer = lx.Lexer.new("=+(){},;5")
+    def test_parse_token(self) -> None:
+        lexer = lx.Lexer.new("=+(){},;5")
 
-    #     test_cases: tuple[tuple[str | None, tk.TokenType], ...] = (
-    #         ("=", tk.TokenType.ASSIGN),
-    #         ("+", tk.TokenType.PLUS),
-    #         ("(", tk.TokenType.LEFT_PARENTHESES),
-    #         (")", tk.TokenType.RIGHT_PARENTHESES),
-    #         ("{", tk.TokenType.LEFT_BRACE),
-    #         ("}", tk.TokenType.RIGHT_BRACE),
-    #         (",", tk.TokenType.COMMA),
-    #         (";", tk.TokenType.SEMICOLON),
-    #         ("5", tk.TokenType.INT),
-    #         (None, tk.TokenType.EOF),
-    #     )
+        test_cases: tuple[tuple[str | None, tk.TokenType], ...] = (
+            ("=", tk.TokenType.ASSIGN),
+            ("+", tk.TokenType.PLUS),
+            ("(", tk.TokenType.LEFT_PARENTHESES),
+            (")", tk.TokenType.RIGHT_PARENTHESES),
+            ("{", tk.TokenType.LEFT_BRACE),
+            ("}", tk.TokenType.RIGHT_BRACE),
+            (",", tk.TokenType.COMMA),
+            (";", tk.TokenType.SEMICOLON),
+            ("5", tk.TokenType.INT),
+            (None, tk.TokenType.EOF),
+        )
 
-    #     for expected_value, expected_type in test_cases:
-    #         token: tk.Token = lexer.next_token()
-    #         actual_value = token.value.decode("ascii") if token.value else token.value
-    #         actual_type = token.type
-    #         with self.subTest():
-    #             self.assertEqual(
-    #                 actual_value,
-    #                 expected_value,
-    #                 f"Expected '{expected_value}, got '{actual_value}'",
-    #             )
-    #             self.assertEqual(
-    #                 actual_type,
-    #                 expected_type,
-    #                 f"Expected '{expected_type}, got '{actual_type}'",
-    #             )
+        for expected_value, expected_type in test_cases:
+            token: tk.Token = lexer.next_token()
+            actual_value = token.value.decode("ascii") if token.value else token.value
+            actual_type = token.type
+            with self.subTest():
+                self.assertEqual(
+                    actual_value,
+                    expected_value,
+                    f"Expected '{expected_value}, got '{actual_value}'",
+                )
+                self.assertEqual(
+                    actual_type,
+                    expected_type,
+                    f"Expected '{expected_type}, got '{actual_type}'",
+                )
 
     def test_parses_script(self) -> None:
         input = read_script()
