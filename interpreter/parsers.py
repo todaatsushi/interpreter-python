feb15cbf7f6cf49af5ea060c1a7ad565e2975ecc
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-02 18:02:56 +0000

    Next token on parser

--- interpreter/parsers.py
+++ interpreter/parsers.py
@@ -18,3 +18,7 @@ class Parser:
         current = lexer.next_token()
         peek = lexer.next_token()
         return cls(lexer=lexer, current_token=current, peek_token=peek)
+
+    def next_token(self) -> None:
+        self.current_token = self.peek_token
+        self.peek_token = self.lexer.next_token()
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -18,3 +18,19 @@ class TestParser(unittest.TestCase):
             parser.peek_token,
             tokens.Token(type=tokens.TokenType.IDENTIFIER, value="x".encode("ascii")),
         )
+
+    def test_next_token(self) -> None:
+        program_input = "let x = 5;"
+        lexer = lexers.Lexer.new(program_input)
+
+        parser = parsers.Parser.new(lexer)
+        parser.next_token()
+
+        self.assertEqual(
+            parser.current_token,
+            tokens.Token(type=tokens.TokenType.IDENTIFIER, value="x".encode("ascii")),
+        )
+        self.assertEqual(
+            parser.peek_token,
+            tokens.Token(type=tokens.TokenType.ASSIGN, value="=".encode("ascii")),
+        )
