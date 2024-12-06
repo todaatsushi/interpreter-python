85ff2ba1c89a5afd7864f9098c7cdd7c2aecaa80
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-06 20:38:25 +0000

    Create env copier

--- interpreter/environment.py
+++ interpreter/environment.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 import dataclasses as dc
 
 from interpreter import objects
@@ -6,13 +9,22 @@ from interpreter import objects
 @dc.dataclass
 class Environment:
     store: dict[str, objects.Object] = dc.field(init=False, default_factory=dict)
+    outer: Environment | None = None
 
     def get(self, name: str) -> tuple[objects.Object | None, bool]:
         try:
             return self.store[name], True
         except KeyError:
-            return None, False
+            pass
+
+        if self.outer:
+            return self.outer.get(name)
+        return None, False
 
     def set(self, name: str, obj: objects.Object) -> objects.Object:
         self.store[name] = obj
         return obj
+
+    @classmethod
+    def new_enclosed(cls, env: Environment) -> Environment:
+        return cls(outer=env)
