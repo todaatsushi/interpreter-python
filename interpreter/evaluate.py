afd665ced3dde50eebddc5003d9c3b0371a732a7
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-06 18:16:25 +0000

    Handle integer infix

--- interpreter/evaluate.py
+++ interpreter/evaluate.py
@@ -27,6 +27,11 @@ def node(to_eval: ast.Node) -> objects.Object:
         case ast.Prefix:
             assert isinstance(to_eval, ast.Prefix) and to_eval.right
             return prefix_expression(to_eval.operator, node(to_eval.right))
+        case ast.Infix:
+            assert isinstance(to_eval, ast.Infix) and to_eval.right and to_eval.left
+            return infix_expression(
+                node(to_eval.left), to_eval.operator, node(to_eval.right)
+            )
         case _:
             logger.error(f"Unhandled type: {type(to_eval)}")
             raise NotImplementedError
@@ -52,6 +57,66 @@ def prefix_expression(operator: str, right: objects.Object) -> objects.Object:
         case _:
             logger.error(f"Unhandled for operator: {operator}")
             raise NotImplementedError
+
+
+def infix_expression(
+    left: objects.Object, operator: str, right: objects.Object
+) -> objects.Object:
+    match operator:
+        case "+":
+            return plus(left, right)
+        case "-":
+            return minus(left, right)
+        case "*":
+            return multiply(left, right)
+        case "/":
+            return divide(left, right)
+        case _:
+            logger.error(f"Unhandled for operator: {operator}")
+            raise NotImplementedError
+
+
+def plus(left: objects.Object, right: objects.Object) -> objects.Object:
+    if isinstance(left, objects.Integer) and isinstance(right, objects.Integer):
+        return objects.Integer(value=left.value + right.value)
+
+    logger.error(
+        f"Unhandled add for these types: {type(left)}(left), {type(right)}(right)"
+    )
+    raise NotImplementedError
+
+
+def minus(left: objects.Object, right: objects.Object) -> objects.Object:
+    if isinstance(left, objects.Integer) and isinstance(right, objects.Integer):
+        return objects.Integer(value=left.value - right.value)
+
+    logger.error(
+        f"Unhandled minus for these types: {type(left)}(left), {type(right)}(right)"
+    )
+    raise NotImplementedError
+
+
+def multiply(left: objects.Object, right: objects.Object) -> objects.Object:
+    if isinstance(left, objects.Integer) and isinstance(right, objects.Integer):
+        return objects.Integer(value=left.value * right.value)
+
+    logger.error(
+        f"Unhandled multiply for these types: {type(left)}(left), {type(right)}(right)"
+    )
+    raise NotImplementedError
+
+
+def divide(left: objects.Object, right: objects.Object) -> objects.Object:
+    """
+    Note: floor, not true divide
+    """
+    if isinstance(left, objects.Integer) and isinstance(right, objects.Integer):
+        return objects.Integer(value=left.value // right.value)
+
+    logger.error(
+        f"Unhandled divide for these types: {type(left)}(left), {type(right)}(right)"
+    )
+    raise NotImplementedError
 
 
 def exclaimation_mark(right: objects.Object) -> objects.Object:
