db03361b7e6daa2d298076669175be5b6cb84e6b
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-06 18:16:25 +0000

    Handle integer infix

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -71,3 +71,26 @@ class TestSelfEvaluating(unittest.TestCase):
         for code, expected in test_cases:
             actual = get_object(code)
             test_self_evaluating_object(self, objects.Integer, actual, expected)
+
+    def test_evaluates_infix_expressions(self) -> None:
+        test_cases: tuple[tuple[str, int], ...] = (
+            ("5", 5),
+            ("10", 10),
+            ("-5", -5),
+            ("-10", -10),
+            ("5 + 5 + 5 + 5 - 10", 10),
+            ("2 * 2 * 2 * 2 * 2", 32),
+            ("-50 + 100 + -50", 0),
+            ("5 * 2 + 10", 20),
+            ("5 + 2 * 10", 25),
+            ("20 + 2 * -10", 0),
+            ("50 / 2 * 2 + 10", 60),
+            ("2 * (5 + 10)", 30),
+            ("3 * 3 * 3 + 10", 37),
+            ("3 * (3 * 3) + 10", 37),
+            ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50),
+        )
+
+        for code, expected in test_cases:
+            actual = get_object(code)
+            test_self_evaluating_object(self, objects.Integer, actual, expected)
