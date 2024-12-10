18a43063f27211d55824d5929d5b7d2bdb37d3ea
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-10 17:43:52 +0000

    Basic compiler def

--- monkey/compiler.py
+++ monkey/compiler.py
@@ -1,2 +1,22 @@
+from __future__ import annotations
+
+import dataclasses as dc
+from typing import TYPE_CHECKING
+
+from monkey import code
+
+if TYPE_CHECKING:
+    from monkey.interpreter import objects
+
+
+@dc.dataclass
 class Compiler:
-    pass
+    instructions: code.Instructions = dc.field(init=False, default_factory=bytes)
+    constants: list[objects.Object] = dc.field(init=False, default_factory=list)
+
+    @classmethod
+    def new(cls) -> Compiler:
+        return cls()
+
+    def compile(self) -> None:
+        raise NotImplementedError
