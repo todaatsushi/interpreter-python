b5d672cda69e5ea9573a7a509352de4456ff8e5b
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-10 16:09:56 +0000

    Make stuff parsable

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import sys
+import enum
 
 from monkey.interpreter import repl
 
 
+class Option(enum.StrEnum):
+    REPL = "repl"
+
+
 def main() -> None:
     arg = sys.argv[1] if len(sys.argv) > 1 else None
-    if arg:
-        if arg == "repl":
-            try:
-                repl.Scanner().start()
-            except (KeyboardInterrupt, EOFError):
-                print("\nBye!")
+    try:
+        opt = Option(arg)
+    except ValueError:
+        opt = None
 
-            return
-
-    print("Usage: python main.py [repl]")
+    if opt:
+        match opt:
+            case Option.REPL:
+                try:
+                    repl.Scanner().start()
+                except (KeyboardInterrupt, EOFError):
+                    print("\nBye!")
+                return
+    else:
+        print("Usage: python main.py [repl]")
 
 
 if __name__ == "__main__":
