5116e44eef3fc352c16b8b78b7346a593f432a04
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-10 16:16:46 +0000

    refactor: make code running into an interface

--- monkey/interpreter/interface.py
+++ monkey/interpreter/interface.py
@@ -1,22 +1,26 @@
-from monkey.interpreter import environment, lexers, parsers, evaluate
+from monkey.interpreter import environment, parsers, evaluate, lexers
 
 
 class Repl:
     PROMPT = ">>>> "
 
     def start(self) -> None:
-        env = environment.Environment()
         while True:
             line = input(self.PROMPT)
             parsed = line.strip().split(self.PROMPT)[0].strip()
-            lexer = lexers.Lexer.new(parsed)
-            parser = parsers.Parser.new(lexer)
+            print(run(parsed))
 
-            program = parser.parse_program()
 
-            if not parser.errors:
-                return_value = evaluate.node(program, env)
-                if return_value:
-                    print(return_value.inspect())
-            else:
-                print("\n".join(parser.errors))
+def run(code: str) -> str:
+    lexer = lexers.Lexer.new(code)
+    parser = parsers.Parser.new(lexer)
+    program = parser.parse_program()
+
+    if not parser.errors:
+        env = environment.Environment()
+        return_value = evaluate.node(program, env)
+        if return_value:
+            return return_value.inspect()
+        return ""
+    else:
+        return "\n".join(parser.errors)
