4ab92d73075b56a6243d111c75dd69fadbd30103
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-10 16:54:59 +0000

    Replace type with enum, define definition and provide lookup func

--- monkey/code.py
+++ monkey/code.py
@@ -1,5 +1,41 @@
+import dataclasses as dc
+import enum
+
 from typing import TypeAlias
 
 
 Instructions: TypeAlias = list[bytes]
-OpCode: TypeAlias = bytes
+
+
+class OpCodeException(Exception):
+    pass
+
+
+class NotFound(OpCodeException):
+    pass
+
+
+class OpCodes(bytes, enum.Enum):
+    CONSTANT = enum.auto()
+
+    def as_int(self) -> int:
+        return int.from_bytes(self)
+
+
+@dc.dataclass(frozen=True, kw_only=True)
+class Definition:
+    name: str
+    operand_widths: list[int]
+
+
+DEFINITIONS: dict[OpCodes, Definition] = {
+    OpCodes.CONSTANT: Definition(name="op_constant", operand_widths=[2])
+}
+
+
+def lookup_byte(op: bytes) -> Definition:
+    try:
+        code = OpCodes(op)
+        return DEFINITIONS[code]
+    except (KeyError, ValueError) as exc:
+        raise NotFound from exc
