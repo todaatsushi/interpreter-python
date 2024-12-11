66477b754c6ac29f7815c2d93c6cf03ea4cd6fa4
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-11 17:36:14 +0000

    Define VM

--- monkey/compiler/vm.py
+++ monkey/compiler/vm.py
@@ -1,17 +1,40 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+import dataclasses as dc
+
+from monkey.compiler import code, compilers
+
 if TYPE_CHECKING:
+    from typing import Final
+
     from monkey.interpreter import objects
+    from monkey.compiler import compilers
 
 
+STACK_SIZE: Final = 2048
+
+
+@dc.dataclass
 class VM:
+    stack_pointer: int
+
+    constants: list[objects.Object]
+    instructions: code.Instructions
+
+    stack: list[objects.Object] = dc.field(default_factory=list)
+
     @classmethod
-    def new(cls) -> VM:
-        raise NotImplementedError
+    def from_bytecode(cls, bytecode: compilers.Bytecode) -> VM:
+        return cls(
+            constants=bytecode.constants,
+            instructions=bytecode.instructions,
+            stack_pointer=0,
+        )
 
     def run(self) -> None:
         raise NotImplementedError
 
+    @property
     def stack_top(self) -> objects.Object:
         raise NotImplementedError
