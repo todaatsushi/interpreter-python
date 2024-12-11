2d4858b99edc8a3071bb09007ea98428e3656eb7
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-11 20:08:02 +0000

    Flag whether or not to run using interpreter or vm

--- main.py
+++ main.py
@@ -10,28 +10,54 @@ class Option(enum.StrEnum):
 
 
 def main() -> None:
-    arg = sys.argv[1] if len(sys.argv) > 1 else None
+    func = sys.argv[1] if len(sys.argv) > 1 else None
     try:
-        opt = Option(arg)
+        opt = Option(func)
     except ValueError:
         opt = None
 
+    match opt:
+        case Option.REPL:
+            if not len(sys.argv) == 3:
+                print("Usage: python main.py [repl] <interpreter|vm>")
+                return
+
+            run_type = sys.argv[2]
+        case Option.RUN:
+            if not len(sys.argv) == 4:
+                print("Usage: python main.py run [filename] <interpreter|vm>")
+                return
+
+            run_type = sys.argv[3]
+        case None:
+            run_type = None
+
     if opt:
+        assert run_type
+        try:
+            rt = interface.RunType(run_type)
+        except ValueError:
+            print(run_type)
+            print(
+                "Usage: python main.py [repl <interpreter|vm>] | [run path <interpreter|vm>]"
+            )
+            return
+
         match opt:
             case Option.REPL:
                 try:
-                    interface.Repl().start()
+                    interface.Repl().start(rt)
                 except (KeyboardInterrupt, EOFError):
                     print("\nBye!")
                 return
             case Option.RUN:
                 if len(sys.argv) < 3:
-                    print("Usage: python main.py run [filename]")
+                    print("Usage: python main.py run [filename] <interpreter|vm>")
                     return
                 file = sys.argv[2]
-                interface.Script().eval(file)
+                interface.Script().eval(file, rt)
     else:
-        print("Usage: python main.py [repl]")
+        print("Usage: python main.py [repl] <interpreter|vm>")
 
 
 if __name__ == "__main__":
--- monkey/interpreter/interface.py
+++ monkey/interpreter/interface.py
@@ -1,24 +1,30 @@
+import enum
 from monkey.interpreter import environment, parsers, evaluate, lexers
+
+
+class RunType(enum.StrEnum):
+    INTERPRETER = "interpreter"
+    COMPILED = "vm"
 
 
 class Repl:
     PROMPT = ">>>> "
 
-    def start(self) -> None:
+    def start(self, run_type: RunType) -> None:
         while True:
             line = input(self.PROMPT)
             parsed = line.strip().split(self.PROMPT)[0].strip()
-            print(run(parsed))
+            print(run(parsed, run_type))
 
 
 class Script:
-    def eval(self, filename: str) -> None:
+    def eval(self, filename: str, run_type: RunType) -> None:
         with open(filename) as f:
             code = f.read()
-            print(run(code))
+            print(run(code, run_type))
 
 
-def run(code: str) -> str:
+def run(code: str, run_type: RunType) -> str:
     lexer = lexers.Lexer.new(code)
     parser = parsers.Parser.new(lexer)
     program = parser.parse_program()
