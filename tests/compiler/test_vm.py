42828342e148fe993ecf9ecd39023cd6eebbfb03
Atsushi Toda <todaatsushi0@gmail.com>
2024-12-11 17:09:32 +0000

    VM test helpers

--- tests/compiler/test_vm.py
+++ tests/compiler/test_vm.py
@@ -1,1 +1,40 @@
+import unittest
+from monkey.compiler import compilers
 
+from monkey.interpreter import objects
+from tests import utils
+
+
+def test_integer_object(
+    tc: unittest.TestCase, expected: int, actual: objects.Object
+) -> None:
+    tc.assertIsInstance(actual, objects.Integer)
+    assert isinstance(actual, objects.Integer)
+
+    tc.assertEqual(expected, actual.value)
+
+
+def test_expected_object(
+    tc: unittest.TestCase, expected: object, actual: objects.Object
+) -> None:
+    with tc.subTest("Testing expected object"):
+        if isinstance(expected, int):
+            return test_integer_object(tc, expected, actual)
+
+
+def run_vm_test(
+    tc: unittest.TestCase, test_cases: tuple[tuple[str, object], ...]
+) -> None:
+    for input_, expected in test_cases:
+        compiler = compilers.Compiler.new()
+        program = utils.parse(input_)
+
+        with tc.subTest(input_):
+            try:
+                compiler.compile(program)
+            except compilers.CouldntCompile as exc:
+                tc.fail(str(exc))
+
+            # New VM
+            # Run VM
+            # Check stack elements
