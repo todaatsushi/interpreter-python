64918187b9c32b26b1373f77a52b335e3b4df5a0
Atsushi Toda <todaatsushi0@gmail.com>
2025-01-04 15:56:30 +0000

    Pass consistent state into repl

--- monkey/interpreter/interface.py
+++ monkey/interpreter/interface.py
@@ -1,6 +1,6 @@
 import enum
-from monkey.compiler import compilers, vm
-from monkey.interpreter import environment, parsers, evaluate, lexers
+from monkey.compiler import compilers, symbol_table, vm
+from monkey.interpreter import environment, objects, parsers, evaluate, lexers
 
 
 class RunType(enum.StrEnum):
@@ -12,10 +12,12 @@ class Repl:
     PROMPT = ">>>> "
 
     def start(self, run_type: RunType) -> None:
+        vm_globals: list[objects.Object | None] = [None] * vm.GLOBALS_SIZE
+        compiler_symbol_table = symbol_table.SymbolTable.new()
         while True:
             line = input(self.PROMPT)
             parsed = line.strip().split(self.PROMPT)[0].strip()
-            print(run(parsed, run_type))
+            print(run(parsed, run_type, vm_globals, compiler_symbol_table))
 
 
 class Script:
@@ -25,7 +27,12 @@ class Script:
             print(run(code, run_type))
 
 
-def run(code: str, run_type: RunType) -> str:
+def run(
+    code: str,
+    run_type: RunType,
+    vm_globals: list[objects.Object | None] | None = None,
+    compiler_symbol_table: symbol_table.SymbolTable | None = None,
+) -> str:
     lexer = lexers.Lexer.new(code)
     parser = parsers.Parser.new(lexer)
     program = parser.parse_program()
@@ -39,9 +46,9 @@ def run(code: str, run_type: RunType) -> str:
         if return_value:
             return return_value.inspect()
     else:
-        compiler = compilers.Compiler.new()
+        compiler = compilers.Compiler.new(compiler_symbol_table)
         compiler.compile(program)
-        machine = vm.VM.from_bytecode(compiler.bytecode())
+        machine = vm.VM.from_bytecode(compiler.bytecode(), vm_globals)
         machine.run()
         print(machine.last_popped_stack_elem.inspect())
 
